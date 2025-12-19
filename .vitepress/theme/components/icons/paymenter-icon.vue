96e575bdf21b9e5254cfbfb6340f18bde4f29bef
iamdarkle <tomasromero@outlook.com>
2025-12-20 00:13:34 +0100

    add: Paymenter icon component

---
+++ .vitepress/theme/components/icons/paymenter-icon.vue
@@ -0,0 +1,47 @@
+<template>
+    <svg
+        width="89"
+        height="121"
+        viewBox="0 0 89 121"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M25.6428 120.5H0.5V63.134L44.5 41.1829V19.2317L0.5 41.1829V0.5H59.167C75.04 0.5 84.471 6.9168 87.46 19.7505L44.5 41.1829V63.134L88.5 41.1829V53.0217C88.5 72.5 78.722 82.239 59.167 82.239H25.6428V120.5Z"
+            fill="url(#paint0_linear_332_30)"
+        />
+        <path
+            d="M44.5 41.1829V63.134L88.5 41.1829V53.0217C88.5 72.5 78.722 82.239 59.167 82.239H25.6428V120.5H0.5V63.134L44.5 41.1829ZM44.5 41.1829L87.46 19.7505C84.471 6.9168 75.04 0.5 59.167 0.5H0.5V41.1829L44.5 19.2317V41.1829Z"
+            stroke="url(#paint1_linear_332_30)"
+        />
+        <defs>
+            <linearGradient
+                id="paint0_linear_332_30"
+                x1="44.5"
+                y1="120.481"
+                x2="44.5"
+                y2="0.538505"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stop-color="white" stop-opacity="0.5" />
+                <stop offset="0.15" stop-color="white" stop-opacity="0.65" />
+                <stop offset="0.3" stop-color="white" stop-opacity="0.78" />
+                <stop offset="0.46" stop-color="white" stop-opacity="0.88" />
+                <stop offset="0.62" stop-color="white" stop-opacity="0.95" />
+                <stop offset="0.8" stop-color="white" stop-opacity="0.99" />
+                <stop offset="1" stop-color="white" />
+            </linearGradient>
+            <linearGradient
+                id="paint1_linear_332_30"
+                x1="44.5"
+                y1="0.500005"
+                x2="44.5"
+                y2="120.5"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stop-color="white" stop-opacity="0" />
+                <stop offset="1" stop-color="white" stop-opacity="0.7" />
+            </linearGradient>
+        </defs>
+    </svg>
+</template>
