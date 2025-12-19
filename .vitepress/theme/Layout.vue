7d7cda2fb8d68970dfa90d4383d211e14eba3946
iamdarkle <tomasromero@outlook.com>
2025-12-20 00:12:10 +0100

    feat: add custom layout footer

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import DefaultTheme from "vitepress/theme";
+import { useData } from "vitepress";
+import LayoutFooter from "./components/LayoutFooter.vue";
+
+const { Layout } = DefaultTheme;
+const { frontmatter } = useData();
+</script>
+
+<template>
+    <Layout>
+        <template #layout-bottom>
+            <LayoutFooter
+                v-if="
+                    frontmatter.layout === 'home' ||
+                    frontmatter.layout === 'page'
+                "
+            />
+        </template>
+    </Layout>
+</template>
